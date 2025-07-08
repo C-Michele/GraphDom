196ef93f0624ba6623d67a411c0e28f62dfd13fd
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-07-08 17:38:48 +0200

    Minor changes

--- labeled_edge_graph.h
+++ labeled_edge_graph.h
@@ -6,14 +6,14 @@
 namespace MAIN_LIBRARY_NAMESPACE {
     template <typename VertexType, typename EdgeLabelType>
     class labeled_edge_graph : virtual public graph<VertexType> {
-    public:
-        ~labeled_edge_graph() override = default;
+        public:
+            ~labeled_edge_graph() override = default;
 
-        [[nodiscard]] virtual const EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) const = 0;
+            [[nodiscard]] virtual const EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) const = 0;
 
-        [[nodiscard]] virtual EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::EDGE_ITERATOR_NAME&) = 0; //TODO: check the signature correctness
-        virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const EdgeLabelType&,  typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::edge_type) = 0;
-        virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, EdgeLabelType&&,  typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::edge_type) = 0;
+            [[nodiscard]] virtual EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::EDGE_ITERATOR_NAME&) = 0; //TODO: check the signature correctness
+            virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const EdgeLabelType&,  typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::edge_type) = 0;
+            virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, EdgeLabelType&&,  typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::edge_type) = 0;
     };
 }
 
