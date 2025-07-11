32abd67071f7f3837e54246023de9ba387033826
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-07-11 21:05:43 +0200

    Added methods implementation for standard cases

--- labeled_edge_graph_base.h
+++ labeled_edge_graph_base.h
@@ -4,7 +4,7 @@
 #include "graph_base.h"
 
 namespace MAIN_LIBRARY_NAMESPACE {
-    template <typename VertexType, typename EdgeLabelType>
+    template <typename VertexType, typename EdgeLabelType, typename T> //TODO: find a better name for "T"
     class labeled_edge_graph_base : virtual public graph_base<VertexType> {
         public:
             ~labeled_edge_graph_base() override = default;
@@ -12,9 +12,19 @@ namespace MAIN_LIBRARY_NAMESPACE {
             [[nodiscard]] virtual const EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) const = 0;
 
             [[nodiscard]] virtual EdgeLabelType& get_edge_label(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::EDGE_ITERATOR_NAME&) = 0; //TODO: check the signature correctness
+            using graph_base<VertexType>::insert_edge;
+            void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME& first, const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME& second, typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::edge_type et) final;
             virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const EdgeLabelType&,  typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::edge_type) = 0;
             virtual void insert_edge(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME&, EdgeLabelType&&,  typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::edge_type) = 0;
     };
+
+    template<typename VertexType, typename EdgeLabelType, typename T> //TODO: find a better name for "T"
+    void labeled_edge_graph_base<VertexType,EdgeLabelType,T>::insert_edge(
+        const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME& first,
+        const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME& second,
+        const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::edge_type et) {
+        insert_edge(first,second,T()(this,first,second),et);
+    }
 }
 
 #endif //LABELED_EDGE_GRAPH_BASE_H
--- labeled_vertex_multi_vertex_graph.h
+++ labeled_vertex_multi_vertex_graph.h
@@ -6,7 +6,7 @@
 #include "multi_vertex_graph.h"
 
 namespace MAIN_LIBRARY_NAMESPACE {
-    template <typename VertexType, typename VertexLabelType>
+    template <typename VertexType, typename VertexLabelType, typename T> //TODO: find a better name for "T"
     class labeled_vertex_multi_vertex_graph : virtual public multi_vertex_graph<VertexType> {
         public:
             ~labeled_vertex_multi_vertex_graph() override = default;
@@ -14,9 +14,22 @@ namespace MAIN_LIBRARY_NAMESPACE {
             [[nodiscard]] virtual const VertexLabelType& get_vertex_label(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::CONSTANT_VERTEX_PTR_NAME&) const = 0;
 
             [[nodiscard]] virtual VertexLabelType& get_vertex_label(const typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME&) = 0; //TODO: check the signature correctness
+            using multi_vertex_graph<VertexType>::insert_vertex;
+            [[nodiscard]] typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME insert_vertex(const VertexType& vertex_to_insert) final;
+            [[nodiscard]] typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME insert_vertex(VertexType&&) final;
             [[nodiscard]] virtual typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME insert_vertex(const std::pair<VertexType,VertexLabelType>&) = 0;
             [[nodiscard]] virtual typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME insert_vertex(std::pair<VertexType,VertexLabelType>&&) = 0;
     };
+
+    template<typename VertexType, typename VertexLabelType, typename T> //TODO: find a better name for "T"
+    typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME labeled_vertex_multi_vertex_graph<VertexType,VertexLabelType,T>::insert_vertex(const VertexType& vertex_to_insert) {
+        return insert_vertex( std::pair( vertex_to_insert,T()(this,vertex_to_insert) ) );
+    }
+
+    template<typename VertexType, typename VertexLabelType, typename T> //TODO: find a better name for "T"
+    typename MAIN_LIBRARY_NAMESPACE::graph_base<VertexType>::VERTEX_PTR_NAME labeled_vertex_multi_vertex_graph<VertexType,VertexLabelType,T>::insert_vertex(VertexType&& vertex_to_insert) {
+        return insert_vertex( std::pair( vertex_to_insert,T()(this,vertex_to_insert) ) );
+    }
 }
 
 #endif //LABELED_VERTEX_MULTI_VERTEX_GRAPH_H
