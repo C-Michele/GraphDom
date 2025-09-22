c4b8e78353e80d8372f0a524d7301372f1267e5a
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-09-22 23:24:42 +0200

    Added macros for generic tests

---
+++ unit_test/generic_graph_tests.h
@@ -0,0 +1,18 @@
+#ifndef GENERIC_GRAPH_TESTS_H
+#define GENERIC_GRAPH_TESTS_H
+
+#include "gtest/gtest.h"
+
+#define NO_VERTICES_AFTER_DEFAULT_CONSTRUCTION_TEST(CONCRETE_CLASS_NAME)    \
+    TEST(CONCRETE_CLASS_NAME, no_vertices_after_default_construction) {     \
+        CONCRETE_CLASS_NAME graph;                                          \
+        EXPECT_EQ(graph.order(),0);                                         \
+    }                                                                       \
+
+#define NO_EDGES_AFTER_DEFAULT_CONSTRUCTION_TEST(CONCRETE_CLASS_NAME)       \
+    TEST(CONCRETE_CLASS_NAME, no_edges_after_default_construction) {        \
+        CONCRETE_CLASS_NAME graph;                                          \
+        EXPECT_EQ(graph.size(),0);                                          \
+    }                                                                       \
+
+#endif //GENERIC_GRAPH_TESTS_H
---
+++ unit_test/generic_set_vertex_graph_tests.h
@@ -0,0 +1,11 @@
+#ifndef GENERIC_SET_VERTEX_GRAPH_TESTS_H
+#define GENERIC_SET_VERTEX_GRAPH_TESTS_H
+
+#include <utility>
+#include <string>
+
+#include "gtest/gtest.h"
+
+//TODO: continue here
+
+#endif //GENERIC_SET_VERTEX_GRAPH_TESTS_H
