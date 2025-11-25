b8424d88e5e502fa9f8319a9cd79e4dc44df1fb3
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-11-25 15:24:26 +0100

    Aliases added

--- multiset_vertex_graph.h
+++ multiset_vertex_graph.h
@@ -24,6 +24,7 @@ namespace MAIN_LIBRARY_NAMESPACE {
             using labeled_directed_edge_endpoint = typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::template labeled_directed_edge_endpoint<VertexContainerPointerType,EdgeLabelType>;
             template <typename EdgeLabelType>
             using labeled_undirected_edge_endpoint = typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::template labeled_undirected_edge_endpoint<VertexContainerPointerType,EdgeLabelType>;
+            using custom_edge_endpoint_less = typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::template custom_edge_endpoint_less<VertexContainerPointerType>;
             using non_mixed_graph_vertex_container = typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::template non_mixed_graph_vertex_container<VertexContainerPointerType>;
             using mixed_graph_vertex_container = typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::template mixed_graph_vertex_container<VertexContainerPointerType>;
             template <typename EdgeLabelType>
