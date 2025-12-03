218fadb7af4b869b8b317a381a9668325c67b363
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2025-12-03 16:04:11 +0100

    Added class 'full_labeled_set_vertex_digraph' (only constructors defined)

---
+++ full_labeled_set_vertex_digraph.h
@@ -0,0 +1,133 @@
+#ifndef FULL_LABELED_SET_VERTEX_DIGRAPH_H
+#define FULL_LABELED_SET_VERTEX_DIGRAPH_H
+
+#include <set>
+
+#include "graph.h"
+#include "set_vertex_graph.h"
+#include "labeled_vertex_graph.h"
+#include "labeled_vertex_set_vertex_graph.h"
+#include "labeled_edge_graph.h"
+#include "labeled_edge_non_mixed_graph.h"
+
+namespace MAIN_LIBRARY_NAMESPACE {
+    template<
+        typename VertexType,
+        typename VertexLabelType,
+        typename EdgeLabelType,
+        typename Compare = std::less<VertexType>,
+        typename T1 = DefaultVertexLabellerType<VertexType,VertexLabelType>, //TODO:: find a better name for T1
+        typename T2 = DefaultEdgeLabellerType<VertexType,EdgeLabelType> // TODO: find a better name for T2
+    >
+    class full_labeled_set_vertex_digraph final :
+    virtual public labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>,
+    virtual public labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2> {
+        public:
+            full_labeled_set_vertex_digraph() = default;
+            full_labeled_set_vertex_digraph(const Compare& v_comp, const T1& v_lab, const T2& e_lab);
+            full_labeled_set_vertex_digraph(const Compare& v_comp, const T1& v_lab, T2&& e_lab = T2());
+            full_labeled_set_vertex_digraph(const Compare& v_comp, T1&& v_lab, const T2& e_lab);
+            explicit full_labeled_set_vertex_digraph(const Compare& v_comp, T1&& v_lab = T1(), T2&& e_lab = T2());
+            full_labeled_set_vertex_digraph(Compare&& v_comp, const T1& v_lab, const T2& e_lab);
+            full_labeled_set_vertex_digraph(Compare&& v_comp, const T1& v_lab, T2&& e_lab = T2());
+            full_labeled_set_vertex_digraph(Compare&& v_comp, T1&& v_lab, const T2& e_lab);
+            explicit full_labeled_set_vertex_digraph(Compare&& v_comp, T1&& v_lab = T1(), T2&& e_lab = T2());
+
+            ~full_labeled_set_vertex_digraph() override; //TODO: look out to memory leaks in ADJs
+
+            [[nodiscard]] std::size_t order() const override;
+            [[nodiscard]] const VertexLabelType& get_vertex_label(const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&) const override;
+            [[nodiscard]] const EdgeLabelType& get_edge_label(const typename graph<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) const override;
+
+            void erase_vertex(const typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&) override; //TODO: look out to memory leaks in ADJ
+            [[nodiscard]] typename MAIN_LIBRARY_NAMESPACE::graph<VertexType>::EDGE_ITERATOR_NAME erase_edge(const typename graph<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) override; //TODO: look out to memory leaks in ADJ
+            [[nodiscard]] VertexLabelType& get_vertex_label(const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&) override;
+            [[nodiscard]] std::pair<typename graph<VertexType>::VERTEX_PTR_NAME,bool> insert_vertex(const VertexType&, const VertexLabelType&) override;
+            [[nodiscard]] std::pair<typename graph<VertexType>::VERTEX_PTR_NAME,bool> insert_vertex(const VertexType&, VertexLabelType&&) override;
+            [[nodiscard]] std::pair<typename graph<VertexType>::VERTEX_PTR_NAME,bool> insert_vertex(VertexType&&, const VertexLabelType&) override;
+            [[nodiscard]] std::pair<typename graph<VertexType>::VERTEX_PTR_NAME,bool> insert_vertex(VertexType&&, VertexLabelType&&) override;
+            [[nodiscard]] EdgeLabelType& get_edge_label(const typename graph<VertexType>::CONSTANT_EDGE_ITERATOR_NAME&) override;
+            void insert_edge(const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const EdgeLabelType&) override;
+            void insert_edge(const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, const typename graph<VertexType>::CONSTANT_VERTEX_PTR_NAME&, EdgeLabelType&&) override;
+        private:
+            class custom_set_less {
+                public:
+                    custom_set_less() = default;
+                    explicit custom_set_less(const Compare& comp) : external_vertex_less_functor(comp){}
+                    explicit custom_set_less(Compare&& comp) : external_vertex_less_functor(std::move(comp)){}
+
+                    bool operator()(
+                        const typename graph<VertexType>::template non_mixed_graph_labeled_vertex_container<set_vertex_graph<VertexType>::VertexContainerPointerType,VertexLabelType>& left,
+                        const typename graph<VertexType>::template non_mixed_graph_labeled_vertex_container<set_vertex_graph<VertexType>::VertexContainerPointerType,VertexLabelType>& right ) const {
+                        return external_vertex_less_functor(left.vertex,right.vertex);
+                    }
+                private:
+                    Compare external_vertex_less_functor;
+            };
+
+            std::set<
+                typename graph<VertexType>::template non_mixed_graph_labeled_vertex_container<set_vertex_graph<VertexType>::VertexContainerPointerType,VertexLabelType>,
+                custom_set_less
+            > vertices;
+    };
+}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(const Compare& v_comp, const T1& v_lab, const T2& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(v_lab),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(e_lab),
+vertices( custom_set_less(v_comp) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(const Compare& v_comp, const T1& v_lab, T2&& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(v_lab),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(std::move(e_lab)),
+vertices( custom_set_less(v_comp) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(const Compare& v_comp, T1&& v_lab, const T2& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(std::move(v_lab)),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(e_lab),
+vertices( custom_set_less(v_comp) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(const Compare& v_comp, T1&& v_lab, T2&& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(std::move(v_lab)),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(std::move(e_lab)),
+vertices( custom_set_less(v_comp) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(Compare&& v_comp, const T1& v_lab, const T2& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(v_lab),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(e_lab),
+vertices( custom_set_less(std::move(v_comp)) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(Compare&& v_comp, const T1& v_lab, T2&& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(v_lab),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(std::move(e_lab)),
+vertices( custom_set_less(std::move(v_comp)) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(Compare&& v_comp, T1&& v_lab, const T2& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(std::move(v_lab)),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(e_lab),
+vertices( custom_set_less(std::move(v_comp)) ) {}
+
+template<typename VertexType, typename VertexLabelType, typename EdgeLabelType, typename Compare, typename T1, typename T2>
+MAIN_LIBRARY_NAMESPACE::full_labeled_set_vertex_digraph<VertexType,VertexLabelType,EdgeLabelType,Compare,T1,T2>::
+full_labeled_set_vertex_digraph(Compare&& v_comp, T1&& v_lab, T2&& e_lab) :
+labeled_vertex_set_vertex_graph<VertexType,VertexLabelType,T1>(std::move(v_lab)),
+labeled_edge_non_mixed_graph<VertexType,EdgeLabelType,T2>(std::move(e_lab)),
+vertices( custom_set_less(std::move(v_comp)) ) {}
+
+//TODO: continue class implementation
+
+#endif //FULL_LABELED_SET_VERTEX_DIGRAPH_H
