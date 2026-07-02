4a31357b14b78e4c2087c86f08dd1f83f0e93d17
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2026-07-02 22:32:00 +0200

    Added a static_assert in 'edge_endpoint' class decl

--- include/graphdom/detail/edge_endpoint.h
+++ include/graphdom/detail/edge_endpoint.h
@@ -1,13 +1,23 @@
 #ifndef GRAPHDOM_EDGE_ENDPOINT_H
 #define GRAPHDOM_EDGE_ENDPOINT_H
 
+#include <type_traits>
+
 #include "../graph.h"
+#include "vertex_container.h"
 
 namespace graphdom {
     template <typename VertexType>
     template <typename VertexContainerPointerType>
     class graph<VertexType>::edge_endpoint {
         public:
+            static_assert(
+                std::is_same< VertexContainerPointerType , graphdom::graph<VertexType>::vertex_container* >::value ||
+                std::is_same< VertexContainerPointerType , const graphdom::graph<VertexType>::vertex_container* >::value
+                ,
+                "The typename 'VertexContainerPointerType' of 'graphdom::graph<VertexType>::edge_endpoint<VertexContainerPointerType>' class must be a pointer to graphdom::graph<VertexType>::vertex_container"
+            );
+
             edge_endpoint() = delete;
             edge_endpoint(const edge_endpoint&) = delete;
             edge_endpoint(edge_endpoint&&) = delete;
