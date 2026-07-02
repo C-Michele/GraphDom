0e0b7e83f32be2c39f4f8e6edcb9d3b3ae76b3dd
Michele Comparini <141649328+C-Michele@users.noreply.github.com>
2026-07-02 22:39:13 +0200

    Definition of 'mixed_graph_labeled_vertex_container' separated from implementation

---
+++ include/graphdom/detail/impl/mixed_graph_labeled_vertex_container.h
@@ -0,0 +1,36 @@
+#ifndef GRAPHDOM_MIXED_GRAPH_LABELED_VERTEX_CONTAINER_IMPL_H
+#define GRAPHDOM_MIXED_GRAPH_LABELED_VERTEX_CONTAINER_IMPL_H
+
+#include "../../graph.h"
+#include "../mixed_graph_vertex_container.h"
+#include "../mixed_graph_labeled_vertex_container.h"
+
+template <typename VertexType>
+template <typename VertexContainerPointerType, typename VertexLabelType>
+graphdom::graph<VertexType>::mixed_graph_labeled_vertex_container<VertexContainerPointerType,VertexLabelType>::
+mixed_graph_labeled_vertex_container(const VertexType& v, const VertexLabelType& vl) :
+    mixed_graph_vertex_container<VertexContainerPointerType>(v),
+    vertex_label(vl) {}
+
+template <typename VertexType>
+template <typename VertexContainerPointerType, typename VertexLabelType>
+graphdom::graph<VertexType>::mixed_graph_labeled_vertex_container<VertexContainerPointerType,VertexLabelType>::
+mixed_graph_labeled_vertex_container(const VertexType& v, VertexLabelType&& vl) :
+    mixed_graph_vertex_container<VertexContainerPointerType>(v),
+    vertex_label(std::move(vl)) {}
+
+template <typename VertexType>
+template <typename VertexContainerPointerType, typename VertexLabelType>
+graphdom::graph<VertexType>::mixed_graph_labeled_vertex_container<VertexContainerPointerType,VertexLabelType>::
+mixed_graph_labeled_vertex_container(VertexType&& v, const VertexLabelType& vl) :
+    mixed_graph_vertex_container<VertexContainerPointerType>(std::move(v)),
+    vertex_label(vl) {}
+
+template <typename VertexType>
+template <typename VertexContainerPointerType, typename VertexLabelType>
+graphdom::graph<VertexType>::mixed_graph_labeled_vertex_container<VertexContainerPointerType,VertexLabelType>::
+mixed_graph_labeled_vertex_container(VertexType&& v, VertexLabelType&& vl) :
+    mixed_graph_vertex_container<VertexContainerPointerType>(std::move(v)),
+    vertex_label(std::move(vl)) {}
+
+#endif //GRAPHDOM_MIXED_GRAPH_LABELED_VERTEX_CONTAINER_IMPL_H
--- include/graphdom/detail/mixed_graph_labeled_vertex_container.h
+++ include/graphdom/detail/mixed_graph_labeled_vertex_container.h
@@ -12,23 +12,17 @@ namespace graphdom {
             mixed_graph_labeled_vertex_container() = delete;
             mixed_graph_labeled_vertex_container(const mixed_graph_labeled_vertex_container&) = delete;
             mixed_graph_labeled_vertex_container(mixed_graph_labeled_vertex_container&&) = delete;
-            mixed_graph_labeled_vertex_container(const VertexType& v, const VertexLabelType& vl) :
-                mixed_graph_vertex_container<VertexContainerPointerType>(v),
-                vertex_label(vl) {}
-            mixed_graph_labeled_vertex_container(const VertexType& v, VertexLabelType&& vl) :
-                mixed_graph_vertex_container<VertexContainerPointerType>(v),
-                vertex_label(std::move(vl)) {}
-            mixed_graph_labeled_vertex_container(VertexType&& v, const VertexLabelType& vl) :
-                mixed_graph_vertex_container<VertexContainerPointerType>(std::move(v)),
-                vertex_label(vl) {}
-            mixed_graph_labeled_vertex_container(VertexType&& v, VertexLabelType&& vl) :
-                mixed_graph_vertex_container<VertexContainerPointerType>(std::move(v)),
-                vertex_label(std::move(vl)) {}
+            mixed_graph_labeled_vertex_container(const VertexType& v, const VertexLabelType& vl);
+            mixed_graph_labeled_vertex_container(const VertexType& v, VertexLabelType&& vl);
+            mixed_graph_labeled_vertex_container(VertexType&& v, const VertexLabelType& vl);
+            mixed_graph_labeled_vertex_container(VertexType&& v, VertexLabelType&& vl);
 
             ~mixed_graph_labeled_vertex_container() = default;
 
             mutable VertexLabelType vertex_label;
     };
 }
+
+#include "impl/mixed_graph_labeled_vertex_container.h"
 
 #endif //GRAPHDOM_MIXED_GRAPH_LABELED_VERTEX_CONTAINER_H
